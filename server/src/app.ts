a9a5db0ee3336831bf00acbbb19156d9b182a6fc
Jimesh-patel <vadodariyajimesh1811@gmail.com>
2026-05-02 20:11:24 +0530

    feat(server): add rate limiting to prevent abuse and improve stability

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import cors from "cors";
 import urlRoutes from "./routes/url.routes";
+import { errorHandler } from "./middleware/error.middleware";
+import { limiter } from "./middleware/rateLimit.middleware";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use(limiter);
+app.use(errorHandler);
 app.use("/api/url", urlRoutes);
 
 export default app;
